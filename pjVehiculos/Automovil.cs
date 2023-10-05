59c1ab9727f33add1825cd41b76e3ac5c764673b
SISTEMAS2-5 <SISTEMAS2-5@DESKTOP-183TUCH>
2023-10-05 11:48:22 -0600

    Agregar archivos de proyecto.

---
+++ pjVehiculos/Automovil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjVehiculos
+{
+    public class Automovil : IVehiculo
+    {
+        private int gasolina;
+        public Automovil(int CantidadLlenarGasolinaInicial)
+        {
+            gasolina = CantidadLlenarGasolinaInicial;
+        }
+
+        public void Conducir()
+        {
+            if (gasolina > 0)
+            {
+                Console.WriteLine("Su Automovil está conduciendo.");
+            }
+            else
+            {
+                Console.WriteLine("El Automovil no tiene suficiente gasolina para poder conducir.");
+            }
+        }
+        public bool LlenarTanque(int CantidadLlenarGasolina)
+        {
+            if(CantidadLlenarGasolina >= 0)
+            {
+                gasolina += CantidadLlenarGasolina;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("La cantidad de gasolina a llenar debe ser mayor o tiene que ser igual a 0.");
+                return false;
+            }
+        }
+    }
+}
---
+++ pjVehiculos/Program.cs
@@ -0,0 +1,13 @@
+﻿using pjVehiculos;
+
+Automovil automovil = new Automovil(0);
+
+Console.WriteLine("Ingrese la cantidad de gasolina que desea para llenar el tanque: ");
+int CantidadLlenarGasolina =  int.Parse(Console.ReadLine());
+
+bool LlenadoRealizado = automovil.LlenarTanque(CantidadLlenarGasolina);
+if (LlenadoRealizado)
+{
+    Console.WriteLine("El tanque de su vehiculo ha sido llenado con éxito!");
+}
+automovil.Conducir();
